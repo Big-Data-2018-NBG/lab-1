97481e781c8899e1ae87f28a949d3df02dd23032
Sailen Nair <sailen.nair@gmail.com>
2018-02-22 23:55:36 +0200

    psuedo-code added, some variable name changes

--- arrayManipulation.c
+++ arrayManipulation.c
@@ -2,23 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdbool.h>
 
-void procedureOne(int *Array, int oneDimArraySize) {
+
+void populateArrayWithZeroes(int *Array, int oneDimArraySize) {
     for (int i = 0; i < oneDimArraySize; ++i) {
         Array[i] = 0;
     }
 }
 
 
-int generateRandomNumber(int Range) {
+int generateRandomNumber(int oneDimArraySize) {
 
-    int randomNumber = rand() % Range;
+    int randomNumber = rand() % oneDimArraySize;
     return randomNumber;
 }
 
 
-void procedureTwo(int *Array, int oneDimArraySize) {
+void uniformPopulationOfOnes(int *Array, int oneDimArraySize) {
 
     int temp = (oneDimArraySize / 10);
     int increment = 0;
@@ -35,49 +35,45 @@ void procedureTwo(int *Array, int oneDimArraySize) {
     }
 }
 
-int *getCoord(int dimension, const int *dimensionArray, int offset) {
 
-    int *coord;
+int *getCoordinate(int kDimension, const int *arrayOfBoundDimensions, int offset) {
+
+    int *coordinate;
     int temp = offset;
-    coord = malloc(dimension * sizeof(int));
+    coordinate = malloc(kDimension * sizeof(int));
 
-    for (int i = dimension - 1; i >= 0; i--) {
-        coord[i] = temp % dimensionArray[i];
-        temp = temp / dimensionArray[i];
+    for (int i = kDimension - 1; i >= 0; i--) {
+        coordinate[i] = temp % arrayOfBoundDimensions[i];
+        temp = temp / arrayOfBoundDimensions[i];
     }
 
-    return coord;
-
+    return coordinate;
 
 }
 
-void procedureThree(int *Array, int oneDimArraySize, const int *dimensionArray, int Dimension) {
 
-    int noOfSamples = oneDimArraySize * 0.05;
+void printUniformRandomCoordinates(int *Array, int oneDimArraySize, const int *arrayOfBoundDimensions, int kDimension) {
+
+    int noOfCoordinates = oneDimArraySize * 0.05;
 
     int *tempArray;
     int *holderArray;
-    holderArray = malloc(noOfSamples * sizeof(int));
+    holderArray = malloc(noOfCoordinates * sizeof(int));
 
+    for (int i = 0; i < noOfCoordinates; ++i) {
+        int randomNumber = generateRandomNumber(oneDimArraySize);
 
-    for (int i = 0; i < noOfSamples; ++i) {
+        for (int l = 0; l < noOfCoordinates; ++l) {
 
-
-        int k = generateRandomNumber(oneDimArraySize);
-
-
-        for (int l = 0; l < noOfSamples; ++l) {
-            if (holderArray[l] == k) {
-                k = generateRandomNumber(oneDimArraySize);
+            if (holderArray[l] == randomNumber) {
+                randomNumber = generateRandomNumber(oneDimArraySize);
             }
-
         }
-        holderArray[i] = k;
+        holderArray[i] = randomNumber;
+        printf("\nValue %d in array is: %d at co-ordinates: ", randomNumber, Array[randomNumber]);
+        tempArray = getCoordinate(kDimension, arrayOfBoundDimensions, randomNumber);
 
-        printf("\nValue %d in array is: %d at co-ordinates: ", k, Array[k]);
-        tempArray = getCoord(Dimension, dimensionArray, k);
-
-        for (int j = 0; j < Dimension; j++) {
+        for (int j = 0; j < kDimension; j++) {
             printf("\t%d", tempArray[j]);
         }
     }
@@ -94,42 +90,43 @@ int main(void) {
 
     while (numberOfDimensions != -1) {
 
-        int *dimensionElementArray;
+        int *arrayOfBoundDimensions;
 
-        dimensionElementArray = (int *) malloc(numberOfDimensions * sizeof(int));
+        arrayOfBoundDimensions = (int *) malloc(numberOfDimensions * sizeof(int));
 
         printf("Please enter the bounds in each dimension of the mapped 1D array:\n");
         for (int i = 0; i < numberOfDimensions; i++) {
-            scanf("%d", &dimensionElementArray[i]);
+            scanf("%d", &arrayOfBoundDimensions[i]);
         }
 
-        int totalSize = 1;
+        int totalSizeOf1Darray = 1;
 
         for (int i = 0; i < numberOfDimensions; i++) {
-            printf("%d\t", dimensionElementArray[i]);
-            totalSize = totalSize * dimensionElementArray[i];
+            printf("%d\t", arrayOfBoundDimensions[i]);
+            totalSizeOf1Darray = totalSizeOf1Darray * arrayOfBoundDimensions[i];
         }
 
-        printf("\nThe total number of elements the 1D array is: %d\n", totalSize);
+        printf("\nThe total number of elements the 1D array is: %d\n", totalSizeOf1Darray);
 
-        int *mainArray;
+        int *mapped1DArray;
 
-        mainArray = (int *) malloc(totalSize * sizeof(int));
+        mapped1DArray = (int *) malloc(totalSizeOf1Darray * sizeof(int));
 
         srand(time(NULL));
 
 
-        procedureOne(mainArray, totalSize);
-        procedureTwo(mainArray, totalSize);
-        procedureThree(mainArray, totalSize, dimensionElementArray, numberOfDimensions);
+        populateArrayWithZeroes(mapped1DArray, totalSizeOf1Darray);
+        uniformPopulationOfOnes(mapped1DArray, totalSizeOf1Darray);
+        printUniformRandomCoordinates(mapped1DArray, totalSizeOf1Darray, arrayOfBoundDimensions,
+                                      numberOfDimensions);
 
 
         printf("\n");
 
 
         int count = 0;
-        for (int i = 0; i < totalSize; ++i) {
-            if (mainArray[i] == 1) {
+        for (int i = 0; i < totalSizeOf1Darray; ++i) {
+            if (mapped1DArray[i] == 1) {
                 count++;
             }
         }
@@ -138,8 +135,9 @@ int main(void) {
         printf("\n");
 
 
-        free(dimensionElementArray);
-        free(mainArray);
+        free(arrayOfBoundDimensions);
+
+        free(mapped1DArray);
 
         printf("Please enter the number of Dimensions or '-1' to quit: ");
         scanf("%d", &numberOfDimensions);
